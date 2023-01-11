709d515855200533d301f88c97e17d0fd243a2b9
Sprin <onesprin@gmail.com>
2023-01-12 00:15:19 +0300

    Update chat.py
    pip install chatgptpy --upgrade

--- src/pychatgpt/classes/chat.py
+++ src/pychatgpt/classes/chat.py
@@ -22,7 +22,32 @@ __hm = Headers.mod
 
 def _called(r, *args, **kwargs):
     if r.status_code == 200 and 'json' in r.headers['Content-Type']:
-        # TODO: Add a way to check if the response is valid
+        # from pychatgpt import Chat, Options
+
+          options = Options()
+
+          # [New] Pass Moderation. https://github.com/rawandahmad698/PyChatGPT/discussions/103
+          # options.pass_moderation = False
+
+          # [New] Enable, Disable logs
+          options.log = True
+
+          # Track conversation
+          options.track = True
+
+          # Use a proxy
+          options.proxies = 'http://localhost:8080'
+
+          # Optionally, you can pass a file path to save the conversation
+          # They're created if they don't exist
+
+          # options.chat_log = "chat_log.txt"
+          # options.id_log = "id_log.txt"
+
+          # Create a Chat object
+          chat = Chat(email="email", password="password", options=options)
+          answer = chat.ask("How are you?")
+          print(answer)TODO: Add a way to check if the response is valid
         pass
 
 
